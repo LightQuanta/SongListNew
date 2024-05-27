c111efe1cfca761127fcd611fd085e49b0cc95a4
Light_Quanta <zch22248@foxmail.com>
2024-05-28 01:31:13 +0800

    将xlsx读取移植到前端

---
+++ src/components/EditorTable.vue
@@ -0,0 +1,80 @@
+<template>
+    <button class="p-5 border" @click="open()">上传xlsx</button>
+    <div v-if="loading">LOADING</div>
+    <table class="flex items-stretch flex-col">
+        <thead>
+            <tr class="flex content-center">
+                <th class="flex-1">歌名</th>
+                <th class="flex-1">原唱</th>
+                <th class="flex-1">语言</th>
+                <th class="flex-1">标签</th>
+                <th class="flex-1">BVID</th>
+                <th class="flex-1">URL</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="flex content-center justify-center text-center" v-for="s in songs" :key="s.name">
+                <td class="flex-1">{{ s.name }}</td>
+                <td class="flex-1">{{ s.artist }}</td>
+                <td class="flex-1">{{ s.language }}</td>
+                <td class="flex-1">{{ s.tags }}</td>
+                <td class="flex-1">{{ s.BVID }}</td>
+                <td class="flex-1">{{ s.url }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useFileDialog } from '@vueuse/core'
+import Excel from "exceljs";
+
+interface SongInfo {
+    name: string;
+    artist: string;
+    language: string;
+    tags?: string;
+    BVID?: string;
+    url?: string;
+}
+
+const { open, onChange } = useFileDialog({
+    multiple: false,
+    accept: '.xlsx',
+})
+
+const loading = ref(false)
+
+onChange(async (files) => {
+    if (files === null) return
+    const file = files[0]
+    loading.value = true
+    
+    const reader = new FileReader()
+    reader.onload = async e => {
+        const buffer = e.target?.result as ArrayBuffer
+
+        const workbook = new Excel.Workbook()
+        await workbook.xlsx.load(new Uint8Array(buffer))
+
+        const worksheet1 = workbook.worksheets[0]
+        songs.value = worksheet1.getRows(2, worksheet1.rowCount)?.map(
+            (row) =>
+                ({
+                    name: row.getCell(1).text,
+                    artist: row.getCell(2).text,
+                    language: row.getCell(3).text,
+                    tags: row.getCell(4).text,
+                    BVID: row.getCell(5).text,
+                    url: row.getCell(6).text,
+                }) as SongInfo,
+        ) ?? []
+        loading.value = false
+    }
+    reader.readAsArrayBuffer(file)
+})
+
+const songs = ref<SongInfo[]>([])
+
+</script>

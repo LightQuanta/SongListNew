9bd77a36dd4fa95b6b0d961b32c783fac152673f
Light_Quanta <zch22248@foxmail.com>
2024-05-28 14:31:00 +0800

    实现自定义excel导入，将歌曲信息修改为更通用的格式

--- src/components/EditorTable.vue
+++ src/components/EditorTable.vue
@@ -1,15 +1,13 @@
 <template>
-    <button class="p-5 border" @click="open()">上传xlsx</button>
-    <div v-if="loading">LOADING</div>
-    <button class="p-5 border" @click="exportToml()" v-if="songs.length > 0">导出toml格式歌单信息</button>
-    <table class="flex items-stretch flex-col">
+    <el-button class="p-5 border" @click="open()">上传xlsx</el-button>
+    <el-button class="p-5 border" @click="exportToml()" v-if="songs.length > 0">导出toml格式歌单信息</el-button>
+    <table class="flex items-stretch flex-col" v-loading="loading">
         <thead>
             <tr class="flex content-center">
                 <th class="flex-1">歌名</th>
                 <th class="flex-1">原唱</th>
                 <th class="flex-1">语言</th>
                 <th class="flex-1">标签</th>
-                <th class="flex-1">BVID</th>
                 <th class="flex-1">URL</th>
             </tr>
         </thead>
@@ -19,26 +17,89 @@
                 <td class="flex-1">{{ s.artist }}</td>
                 <td class="flex-1">{{ s.language }}</td>
                 <td class="flex-1">{{ s.tags }}</td>
-                <td class="flex-1">{{ s.BVID }}</td>
-                <td class="flex-1">{{ s.url }}</td>
+                <td class="flex-1 text-nowrap overflow-hidden">{{ s.links }}</td>
             </tr>
         </tbody>
     </table>
+    <el-dialog v-model="showDialog" title="选择每列类型" center align-center modal :close-on-click-modal="false"
+        :close-on-press-escape="false" :show-close="false">
+        <el-table :data="rowTitleData" stripe class=" text-center">
+            <el-table-column property="id" label="列号" width="100" />
+            <el-table-column property="name" label="名称" width="150" />
+            <el-table-column property="dataType" label="类型">
+                <template #default="scope">
+                    <el-select v-model="rowTitleData[scope.$index].dataType">
+                        <el-option v-for="key in Object.keys(titleTypesAndDesc)" :value="key"
+                            :label="titleTypesAndDesc[key]"
+                            :disabled="key != rowTitleData[scope.$index].dataType && selectedRows.includes(key)">
+                        </el-option>
+                    </el-select>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-button @click="showDialog = false; waitForDialog?.(false)">取消</el-button>
+        <el-button @click="verifyTypes">确定</el-button>
+    </el-dialog>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useFileDialog } from '@vueuse/core'
 import Excel from "exceljs";
 import { stringify as toToml } from 'smol-toml'
 import type { SongInfo } from '../types'
+
+import 'element-plus/dist/index.css'
+import { ElDialog, ElButton, ElTable, ElTableColumn, ElSelect, ElOption, ElMessage } from 'element-plus'
 
 const { open, onChange } = useFileDialog({
     multiple: false,
     accept: '.xlsx',
 })
 const loading = ref(false)
+let waitForDialog = ref<(value: boolean) => void>()
 
+type DataType = keyof SongInfo | 'BVID' | 'neteaseRadio' | 'ignore'
+interface ExcelTitleInfo {
+    id: number
+    name: string
+    dataType: DataType
+}
+
+const titleTypesAndDesc: Record<string, string> = {
+    'ignore': '忽略该列内容',
+    'id': '歌曲序号',
+    'name': '歌名',
+    'artist': '原唱',
+    'language': '语言',
+    'alias': '歌曲别名',
+    'tags': '歌曲标签（可填写多个，用逗号或分号分隔）',
+    'remark': '歌曲备注',
+    'paid': '是否为付费歌曲',
+    'top': '是否为置顶歌曲',
+    'sc': '点歌需要该列标注金额的sc',
+    'BVID': '歌切BV号（可填写多个，用逗号或分号分隔）',
+    'links': '完整链接（如歌切等）',
+    'date': '该曲唱过的日期（可填写多个，用逗号或分号分隔）',
+    'neteaseRadio': '网易云播客ID',
+}
+
+const showDialog = ref(false)
+const rowTitleData = ref<ExcelTitleInfo[]>([])
+const selectedRows = computed(() => rowTitleData.value.map(r => r.dataType.toString()).filter(t => t !== 'ignore'))
+
+const verifyTypes = () => {
+    const set = new Set(rowTitleData.value.map(d => d.dataType))
+    if (set.has('name') && set.has('artist') && set.has('language')) {
+        waitForDialog.value?.(true)
+        loading.value = false
+        showDialog.value = false
+    } else {
+        ElMessage.error('至少需要选择歌名、原唱和语言！')
+    }
+}
+
+// 文件成功上传
 onChange(async (files) => {
     if (files === null) return
     const file = files[0]
@@ -47,29 +108,126 @@ onChange(async (files) => {
     const reader = new FileReader()
     reader.onload = async e => {
         const buffer = e.target?.result as ArrayBuffer
-
         const workbook = new Excel.Workbook()
         await workbook.xlsx.load(new Uint8Array(buffer))
 
         const worksheet1 = workbook.worksheets[0]
-        songs.value = worksheet1.getRows(2, worksheet1.rowCount)?.map(
-            (row) => {
-                const song: SongInfo = {
-                    name: row.getCell(1).text.trim(),
-                    artist: row.getCell(2).text.trim(),
-                    language: row.getCell(3).text.trim(),
-                };
-                row.getCell(4).text.trim() !== '' && (song.tags = row.getCell(4).text);
-                row.getCell(5).text.trim() !== '' && (song.BVID = row.getCell(5).text);
-                row.getCell(6).text.trim() !== '' && (song.url = row.getCell(6).text);
-                return song
+        rowTitleData.value = []
+
+        // 尝试推断首行定义
+        worksheet1.getRow(1).eachCell((c, index) => {
+            const text = c.toString().trim().toLowerCase()
+            if (text === '') return
+
+            const titleInfo: ExcelTitleInfo = {
+                id: index,
+                name: text,
+                dataType: 'ignore'
             }
-        ).filter(s => s.name.trim() !== '') ?? []
+
+            if (text === '序号' || text === 'id') {
+                titleInfo.dataType = 'id'
+            } else if (text === '歌名' || text === '名称') {
+                titleInfo.dataType = 'name'
+            } else if (text.includes('翻译')) {
+                titleInfo.dataType = 'alias'
+            } else if (text.includes('原唱') || text.includes('歌手')) {
+                titleInfo.dataType = 'artist'
+            } else if (text === '语言' || text === '语种') {
+                titleInfo.dataType = 'language'
+            } else if (text === '标签' || text === '分类' || text === '类别' || text === '备注') {
+                titleInfo.dataType = 'tags'
+            } else if (text.includes('歌切') || text.includes('bv')) {
+                titleInfo.dataType = 'BVID'
+            } else if (text.includes('网易云播客') || text.includes('网易云电台')) {
+                titleInfo.dataType = 'neteaseRadio'
+            } else if (text.includes('日期')) {
+                titleInfo.dataType = 'date'
+            } else if (text.includes('置顶')) {
+                titleInfo.dataType = 'top'
+            } else if (text.includes('付费')) {
+                titleInfo.dataType = 'paid'
+            } else if (text.includes('sc')) {
+                titleInfo.dataType = 'sc'
+            }
+            rowTitleData.value.push(titleInfo)
+        })
+
+        // 让用户选择首行定义
+        showDialog.value = true
+        const result = new Promise<boolean>((resolve) => {
+            waitForDialog.value = resolve
+        })
+
+        // 如果对话框中点击取消则退出
+        if (!await result) {
+            loading.value = false
+            return
+        }
+
+        // 处理剩余内容
+        songs.value = worksheet1.getRows(2, worksheet1.rowCount)?.map(
+            (row, index) => {
+                const song: SongInfo = {} as SongInfo
+                let hasId = false
+                // 按照首行定义处理每一列
+                for (let i = 0; i < row.cellCount; i++) {
+                    if (rowTitleData.value[i] !== undefined) {
+                        const text = row.getCell(i + 1).toString().trim()
+                        if (text === '') continue
+                        switch (rowTitleData.value[i].dataType) {
+                            case 'id': song.id = parseInt(text); hasId = true; break;
+                            case 'name': song.name = text; break;
+                            case 'artist': song.artist = text; break;
+                            case 'alias': song.alias?.push(text) ?? (song.alias = [text]); break;
+                            case 'language': song.language = text; break;
+                            case 'tags': {
+                                const tags = text.replaceAll('，', ',')
+                                    .replaceAll('；', ';')
+                                    .replaceAll(';', ',')
+                                    .split(',')
+                                    .map(t => t.trim())
+                                song.tags?.push(...tags) ?? (song.tags = tags);
+                                break;
+                            }
+                            case 'BVID': {
+                                const bvs = text.replaceAll('，', ',')
+                                    .replaceAll('；', ';')
+                                    .replaceAll(';', ',')
+                                    .split(',')
+                                    .map(v => `https://www.bilibili.com/video/${v.trim()}`)
+
+                                song.links?.push(...bvs) ?? (song.links = bvs);
+                                break;
+                            }
+                            case 'neteaseRadio': song.links?.push(`https://music.163.com/#/dj?id=${text}`) ?? (song.links = [`https://music.163.com/#/dj?id=${text}`]); break;
+                            case 'date': {
+                                const date = text.replaceAll('，', ',')
+                                    .replaceAll('；', ';')
+                                    .replaceAll(';', ',')
+                                    .split(',')
+                                    .map(d => d.trim())
+                                song.date?.push(...date) ?? (song.date = date);
+                                break;
+                            }
+                            case 'top': if (text === '1') song.top = true; break;
+                            case 'paid': if (text === '1') song.paid = true; break;
+                            case 'sc': song.sc = +text; break;
+                            case 'links': song.links?.push(text) ?? (song.links = [text]); break;
+                        }
+                    }
+                }
+                // 未定义ID列则使用自增index
+                if (!hasId) song.id = index +1
+                return (song.name !== undefined && song.name !== '') ? song : null
+            }
+        ).filter(s => s !== null && s?.name.trim() !== '') as SongInfo[] ?? []
         loading.value = false
     }
     reader.readAsArrayBuffer(file)
 })
 
+// 将歌单导出为toml配置文件
 const exportToml = () => {
     const toml = toToml({ songs: songs.value })
     const blob = new Blob([toml], { type: 'text/plain;charset=utf-8' })
@@ -81,5 +239,4 @@ const exportToml = () => {
 }
 
 const songs = ref<SongInfo[]>([])
-
 </script>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,16 @@
-// TODO　将歌曲信息修改为更通用的格式
 export interface SongInfo {
-    name: string;
-    artist: string;
-    language: string;
-    tags?: string;
+    id: number
+    name: string
+    artist: string
+    language: string
+    alias?: string[]
+    tags?: string[]
+    remark?: string
 
-    BVID?: string;
-    url?: string;
+    paid?: boolean
+    top?: boolean
+    sc?: number
 
-    // links?: string[];
+    date?: string[]
+    links?: string[]
 }
